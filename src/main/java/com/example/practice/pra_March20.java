18b2a07cbc82473a6a1803fd5a02b11620c4aa0d
AppleHowMuch <SecreTary_RC@hotmail.com>
2023-03-20 11:40:08 +0800

    String.indexof practice

--- src/main/java/com/example/practice/pra_March20.java
+++ src/main/java/com/example/practice/pra_March20.java
@@ -1,7 +1,5 @@
 package com.example.practice;
 
-
-
 import ch.qos.logback.core.recovery.ResilientSyslogOutputStream;
 
 //@SpringBootApplication
@@ -9,14 +7,19 @@ public class pra_March20 {
 
 	public static void main(String[] args) {
 		String skyDra = "明教教主張無忌是武當的弟子，但明教的教徒不允許他回武當，要求掌管明教";
-		System.out.print(skyDra.indexOf("明教"));
-		System.out.println(skyDra.length());
-		int numIndex=0 , y = 0;
-		for (int i = 0; i<=skyDra.length();i++) {
-			
+		String key = "明教";
+		int numIndex = 0, count = 0;
+		for (;;) {
+			numIndex = skyDra.indexOf("明教", numIndex);
+			if (numIndex == -1) {
+				System.out.println("出現 "+count+" 次");
+				break;
+			}
+			System.out.println(numIndex);
+			numIndex += key.length();
+			count++;
 		}
 
 	}
-	
-	
+
 }
